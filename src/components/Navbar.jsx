1928e76a4efbd7cfbc78188b42a4f4fba4b24564
Matthew Pandolfo <matt@pandolfo.com>
2023-01-13 08:05:39 -0500

    desktop view navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,32 @@
 import { Burger } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
 import React, { useState } from "react";
 import logo from "../assets/maneeley-logo.png";
 import AppDrawer from "./Drawer";
 
 function Navbar() {
   const [isOpened, setIsOpened] = useState(false);
+  const view = useMediaQuery("(min-width: 900px)");
 
   return (
     <>
       <header className="navbar">
-        <div className="navbar-content">
+        <div className={!view ? "navbar-content-mobile" : "navbar-content"}>
           <img src={logo} />
-          <Burger
-            opened={isOpened}
-            onClick={() => setIsOpened(!isOpened)}
-            title="Nav"
-            color="white"
-          />
+          {!view && (
+            <Burger
+              opened={isOpened}
+              onClick={() => setIsOpened(!isOpened)}
+              title="Nav"
+              color="white"
+            />
+          )}
+          <ul>
+            <li>Home</li>
+            <li>Weddings</li>
+            <li>Catering</li>
+            <li>Contact</li>
+          </ul>
         </div>
       </header>
       <AppDrawer
