19033df3b67c60034f9e3e5f5983131ba53c4138
Matthew Pandolfo <matt@pandolfo.com>
2023-01-15 21:07:29 -0500

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,24 @@
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faFax, faSquarePhone } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 function Footer() {
   return (
     <div className="footer">
       <hr />
+      <div className="footer-content">
+        <div className="footer-icons">
+          <FontAwesomeIcon icon={faSquarePhone} size="xl" title="Phone" />
+          <FontAwesomeIcon icon={faFax} size="xl" title="Fax" />
+          <FontAwesomeIcon icon={faFacebook} size="xl" title="Facebook" />
+        </div>
+        <h6>Helpful Links</h6>
+        <p>Catering | Contact</p>
+        <p className="copyright">
+          Copyright &copy; 2023 Maneeley's Banquet & Catering
+        </p>
+      </div>
     </div>
   );
 }
