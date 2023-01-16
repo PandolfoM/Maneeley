ae85b8b6b22d28ad30c62834cff9ca948f7dc7d5
Matthew Pandolfo <matt@pandolfo.com>
2023-01-16 14:22:35 -0500

    accordation for menus

--- src/pages/Catering.jsx
+++ src/pages/Catering.jsx
@@ -1,3 +1,4 @@
+import { Accordion, createStyles } from "@mantine/core";
 import React from "react";
 import { Link } from "react-router-dom";
 import Separator from "../components/Separator";
@@ -6,25 +7,43 @@ function Catering() {
   return (
     <div className="catering">
       <Separator title={"Catering"} />
-      <p>
-        Whatever the occasion, Maneeley’s Catering executes each event with
-        skill, pride and professionalism.
-      </p>
-      <p>
-        Whether you’re looking for a full service dinner, cocktail party or back
-        yard cook out, Maneeley’s Catering is always ready to assist in planning
-        that special day!
-      </p>
-      <p>
-        At Maneeley’s Catering, we offer menus for a variety of celebrations and
-        events. If you are looking to have an event catered at your place of
-        business or to simply order lunch for your staff please view our menus
-        below.
-      </p>
-      <p>
-        For any questions on our catering menu, please{" "}
-        <Link to="/contact">contact us today.</Link>
-      </p>
+      <div>
+        <p>
+          Whatever the occasion, Maneeley’s Catering executes each event with
+          skill, pride and professionalism.
+        </p>
+        <p>
+          Whether you’re looking for a full service dinner, cocktail party or
+          back yard cook out, Maneeley’s Catering is always ready to assist in
+          planning that special day!
+        </p>
+        <p>
+          At Maneeley’s Catering, we offer menus for a variety of celebrations
+          and events. If you are looking to have an event catered at your place
+          of business or to simply order lunch for your staff please view our
+          menus below.
+        </p>
+        <p>
+          For any questions on our catering menu, please{" "}
+          <Link to="/contact">contact us today.</Link>
+        </p>
+      </div>
+      <div className="catering-menus">
+        <Accordion variant="filled">
+          <Accordion.Item value="cateringMenus">
+            <Accordion.Control>Catering Menus</Accordion.Control>
+            <Accordion.Panel>Catering Menus Data</Accordion.Panel>
+          </Accordion.Item>
+          <Accordion.Item value="corporateMenus">
+            <Accordion.Control>Corporate Catering Menus</Accordion.Control>
+            <Accordion.Panel>Corporate Catering Menus Data</Accordion.Panel>
+          </Accordion.Item>
+          <Accordion.Item value="holidayMenus">
+            <Accordion.Control>Holiday Catering Menus</Accordion.Control>
+            <Accordion.Panel>Holiday Catering Menus Data</Accordion.Panel>
+          </Accordion.Item>
+        </Accordion>
+      </div>
     </div>
   );
 }
