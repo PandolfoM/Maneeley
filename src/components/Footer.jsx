2d6bb12a3b09120a7c87f5979223781de74c53d8
Matthew Pandolfo <matt@pandolfo.com>
2023-01-16 08:34:19 -0500

    background and footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 import { faFax, faSquarePhone } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ActionIcon } from "@mantine/core";
 import React from "react";
 
 function Footer() {
@@ -8,10 +9,16 @@ function Footer() {
     <div className="footer">
       <hr />
       <div className="footer-content">
-        <div className="footer-icons">
-          <FontAwesomeIcon icon={faSquarePhone} size="xl" title="Phone" />
-          <FontAwesomeIcon icon={faFax} size="xl" title="Fax" />
-          <FontAwesomeIcon icon={faFacebook} size="xl" title="Facebook" />
+        <div className="icons">
+          <a href="/">
+            <FontAwesomeIcon icon={faSquarePhone} size="xl" title="Phone" />
+          </a>
+          <a href="/">
+            <FontAwesomeIcon icon={faFax} size="lg" title="Fax" />
+          </a>
+          <a href="/">
+            <FontAwesomeIcon icon={faFacebook} size="lg" title="Facebook" />
+          </a>
         </div>
         <h6>Helpful Links</h6>
         <p>Catering | Contact</p>
