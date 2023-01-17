61f83eabd0d4c301f82794a964f3bcc613ebbdfe
Matthew Pandolfo <matt@pandolfo.com>
2023-01-17 18:32:02 -0500

    start of mobile dash

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,33 +1,50 @@
 import { signOut } from "firebase/auth";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import Page from "../components/Page";
-import Separator from "../components/Separator";
-import { auth } from "../firebase";
 import { AuthContext } from "../auth/context";
+import { ActionIcon } from "@mantine/core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClipboard, faUsers } from "@fortawesome/free-solid-svg-icons";
 
 function Dashboard() {
-  const { setCurrentUser } = useContext(AuthContext);
+  const { currentUser, setCurrentUser } = useContext(AuthContext);
+  const [currentDash, setCurrentDash] = useState("menus");
   const navigate = useNavigate();
 
+  // {
+  //   {
+  //     menus: "Hello World",
+  //     users: "Hello World 2",
+  //   }[currentDash]
+  // }
+
   return (
-    <Page>
-      <Separator title={"Dashboard"} />
-      <button
-        onClick={() =>
-          signOut(auth)
-            .then(() => {
-              navigate("/");
-              setCurrentUser();
-            })
-            .catch((e) => {
-              console.log(e);
-            })
-        }>
-        Sign Out
-      </button>
-    </Page>
+    <div className="dashboard">
+      <div className="dashboard-nav">
+        <ActionIcon
+          variant="transparent"
+          onClick={() => setCurrentDash("menus")}>
+          <FontAwesomeIcon
+            icon={faClipboard}
+            size="xl"
+            color={currentDash === "menus" ? "#fffcf1" : "inherit"}
+          />
+        </ActionIcon>
+        <ActionIcon
+          variant="transparent"
+          onClick={() => setCurrentDash("users")}>
+          <FontAwesomeIcon
+            icon={faUsers}
+            size="xl"
+            color={currentDash === "users" ? "#fffcf1" : "inherit"}
+          />
+        </ActionIcon>
+      </div>
+      <div className="dashboard-content">
+        <p>Hello World</p>
+      </div>
+    </div>
   );
 }
 
