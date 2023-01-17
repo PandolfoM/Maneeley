51101ba92eca49e748ca58e455f0095658b17a7e
Matthew Pandolfo <matt@pandolfo.com>
2023-01-17 11:08:00 -0500

    firebase sign in

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Page from "../components/Page";
+import Separator from "../components/Separator";
+
+function Dashboard(props) {
+  return (
+    <Page>
+      <Separator title={"Dashboard"} />
+    </Page>
+  );
+}
+
+export default Dashboard;
