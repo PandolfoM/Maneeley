0eb7d596e26bda4fb1a98ed138ca3041cb521598
Matthew Pandolfo <matt@pandolfo.com>
2023-03-07 10:25:41 -0500

    remove notice

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import About from "../components/About";
 import Hero from "../components/Hero";
-import Notice from "../components/Notice";
 import Services from "../components/Services";
 
 function Home() {
   return (
     <>
       <Hero />
-      <Notice />
       <Services />
       <About />
     </>
