22db90a2c59ddff3fad9a17c523769c91812f4f5
Matthew Pandolfo <matt@pandolfo.com>
2023-03-17 18:20:39 -0400

    fix env

--- src/firebase.js
+++ src/firebase.js
@@ -4,8 +4,6 @@ import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
 import { getStorage } from "firebase/storage";
 
-console.log(import.meta.env.APIKEY);
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APIKEY,
   authDomain: import.meta.env.VITE_AUTHDOMAIN,
--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,4 @@ dotenv.config();
 export default defineConfig({
   plugins: [react()],
   base: "/Maneeley/",
-  define: {
-    "import.meta.env.VITE_APIKEY": JSON.stringify(process.env.APIKEY),
-    "import.meta.env.VITE_AUTHDOMAIN": JSON.stringify(process.env.AUTHDOMAIN),
-    "import.meta.env.VITE_PROJECTID": JSON.stringify(process.env.PROJECTID),
-    "import.meta.env.VITE_STORAGEBUCKET": JSON.stringify(
-      process.env.STORAGEBUCKET
-    ),
-    "import.meta.env.VITE_MESSAGINGSENDERID": JSON.stringify(
-      process.env.MESSAGINGSENDERID
-    ),
-    "import.meta.env.APPID": JSON.stringify(process.env.APPID),
-  },
 });
