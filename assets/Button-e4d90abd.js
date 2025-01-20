8b30b19b868a6f316fff05ce8f9c0a2b47a8fd9b
PandolfoM <PandolfoM@users.noreply.github.com>
2025-01-20 23:04:03 +0000

    deploy: 6a47d5baa027000c236072308e007a2d55093df9

--- assets/Button-e4d90abd.js
+++
@@ -1,1 +0,0 @@
-import{d as a,al as e}from"./index-461fd374.js";function d({name:r,...t}){return a(e,{variant:"gradient",gradient:{from:"#b17900",to:"#fdbb2d",deg:360},radius:"xl",uppercase:!0,...t,children:r})}export{d as A};
