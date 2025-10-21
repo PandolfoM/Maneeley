2c6480167b114a1b229b9ecc463e05bf4701acf4
Matthew Pandolfo <matt@pandolfo.com>
2025-10-21 19:48:22 -0400

    add holiday menu to hero image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import AppButton from "./Button";
 
 function Hero() {
   return (
@@ -9,6 +10,12 @@ function Hero() {
             <h1>
               Welcome to <span>Maneeley's</span>
             </h1>
+            <a
+              href="https://firebasestorage.googleapis.com/v0/b/maneeley.appspot.com/o/Holiday%2F283d5b80-86ca-47dc-aa6f-58f31b490d8d?alt=media&token=697d67a1-d5bd-4275-88ce-514830e3da16"
+              target="_blank"
+            >
+              <AppButton name={"View our 2025 holiday banquet menu"} />
+            </a>
           </div>
         </div>
       </div>
